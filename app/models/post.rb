5f04f8e9cded31869b7d4a4c595847e26aa13d98
Peter Kreppein <pmkreppein@gmail.com>
2018-12-19 05:45:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::
+class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
