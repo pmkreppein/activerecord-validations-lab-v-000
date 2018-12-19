6050203f764a81cc38c39cd756980371ec11551d
Peter Kreppein <pmkreppein@gmail.com>
2018-12-19 05:44:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
-  validates :summary, length: { maximum: 250 
+  validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :is_clickbait?
     
